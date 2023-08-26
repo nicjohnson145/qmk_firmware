cc4e51befa025ed91ac181b7b0e3b5dd390efe01
Nic Johnson <nicjohnson145@hotmail.com>
2023-08-25 19:43:17 -0500

    feat: already defaulted apparently

--- keyboards/crkbd/rules.mk
+++ keyboards/crkbd/rules.mk
@@ -1,14 +1,9 @@
 # Build Options
 #   change yes to no to disable
 #
-
 DEFAULT_FOLDER = crkbd/rev1
 
 RGBLIGHT_SUPPORTED = yes
 RGB_MATRIX_SUPPORTED = yes
 
 DYNAMIC_MACRO_ENABLE = yes
-
-OLED_ENABLE = yes
-LTO_ENABLE = yes
-RGBLIGHT_ENABLE = yes
